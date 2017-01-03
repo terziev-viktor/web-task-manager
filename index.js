5a8476f1bb18a253df28b43775b987f594c0834f
Viktor Terziev <Viktor Terziev>
2017-01-03 23:48:28 +0200

    Added the mane hage of the app.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require('express'),
+    app = express();
+
+app.use('/scripts', express.static(__dirname + '/bower_components/'));
+app.use('/styles', express.static(__dirname + '/css/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/' + 'home.html');
+})
+
+app.listen(27017, () => {
+    console.log('Listeting at port 27017')
+})
