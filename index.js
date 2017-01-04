98c84cf2773a9026d3b31bfc3b6f5c6b5c4d1b01
Viktor Terziev <Viktor Terziev>
2017-01-05 01:00:59 +0200

    Added view for creating a new task.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express'),
-    app = express();
+    $ = require('jquery'),
+    app = express(),
+    Mustache = require('mustache');
 
 app.use('/scripts', express.static(__dirname + '/bower_components/'));
 app.use('/styles', express.static(__dirname + '/css/'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/' + 'home.html');
+    
 })
 
 app.listen(27017, () => {
