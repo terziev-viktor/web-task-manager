b6f325f2a71811bfd228f44ba4298a603f68bb1d
Viktor Terziev <Viktor Terziev>
2017-01-09 13:28:54 +0200

    Sql query for creating function 'GetUserByUsername' added.

---
+++ database/Programmability/Functions/GetUserByUsername.sql
@@ -0,0 +1,22 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Inline Function (New Menu).SQL
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION GetUserByUsername
+(	
+	-- Add the parameters for the function here
+	@Username NVARCHAR(128)
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	-- Add the SELECT statement with parameter references here
+	SELECT TOP 1 U.Username, U.[Password] FROM Users AS U WHERE U.Username = @Username
+)
+GO
