17d5b1c815544f58949d5edfb277d630ad122d73
Viktor Terziev <Viktor Terziev>
2017-01-25 17:42:15 +0200

    Deleted test insertions.

--- database/Entity Framework/WebTaskManager.Data/Program.cs
+++ database/Entity Framework/WebTaskManager.Data/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using WebTaskManager.Models;
 
 namespace WebTaskManager.Data
@@ -10,30 +7,7 @@ namespace WebTaskManager.Data
     {
         static void Main(string[] args)
         {
-            using (WebTaskManagerDb context = new WebTaskManagerDb())
-            {
-                context.Database.Log = Console.Write;
-
-                context.Comments.Add(new Comment()
-                {
-                    Content = "Very nice task",
-                    Author = context.Users.Find("MITKO"),
-                    Date = DateTime.Now,
-                    Task = new Task()
-                    {
-                        Title = "Go",
-                        Description = "To the church",
-                        Creator = context.Users.Find("Yordan"),
-                        Deadline = new DateTime(2017, 3, 11),
-                        IsDone = false,
-                        Priority = Models.Enums.TaskPriority.LOW,
-                        Repeatability = Models.Enums.TaskRepeatability.WEEKLY,
-                        Progress = 20
-                    }
-                });
-
-                context.SaveChanges();
-            }
+            
         }
     }
 }
