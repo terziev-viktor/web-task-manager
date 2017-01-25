074bfb2069135923c34444f85e357b7488da09cf
Viktor Terziev <Viktor Terziev>
2017-01-25 08:38:13 +0200

    Anti sql injection middleware.

--- api.js
+++ api.js
@@ -1,10 +1,35 @@
 module.exports = (app, db) => {
-    const auth = (req, ers, next) => {
+    const auth = (req, res, next) => {
         if (!req.isAuthenticated()) {
-            res.send(401);
+            res.sendStatus(401);
         } else {
             next();
         }
+    }
+
+    const sql_inj = (req, res, next) => {
+        let sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
+        if (sql_meta.test(value)) {
+            res.sendStatus(403);
+        }
+
+        let sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
+        if (sql_meta2.test(value)) {
+            res.sendStatus(403);
+        }
+
+        let sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
+        if (sql_typical.test(value)) {
+            res.sendStatus(403);
+        }
+
+        let sql_union = new RegExp('((%27)|(\'))union', 'i');
+        if (sql_union.test(value)) {
+            res.sendStatus(403);
+        }
+
+        return true;
+        
     }
 
     app.get('/', (req, res) => {
--- database.js
+++ database.js
@@ -5,32 +5,6 @@ module.exports = class Database {
         this.config = config;
     }
 
-
-    // Prevents sql injections
-    validateParam(value) {
-        let sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
-        if (sql_meta.test(value)) {
-            return false;
-        }
-
-        let sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
-        if (sql_meta2.test(value)) {
-            return false;
-        }
-
-        let sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
-        if (sql_typical.test(value)) {
-            return false;
-        }
-
-        let sql_union = new RegExp('((%27)|(\'))union', 'i');
-        if (sql_union.test(value)) {
-            return false;
-        }
-
-        return true;
-    }
-
     request(query, cb) {
 
         let conn = new sql.Connection(this.config)
