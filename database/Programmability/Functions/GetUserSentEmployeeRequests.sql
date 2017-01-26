326225c5bdf6043020f1e39af814db363d9847a8
Viktor Terziev <Viktor Terziev>
2017-01-26 21:33:20 +0200

    Added GetUserSentEmployeeRequests.

---
+++ database/Programmability/Functions/GetUserSentEmployeeRequests.sql
@@ -0,0 +1,22 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE FUNCTION GetUserSentEmployeeRequests
+(
+	@Username NVARCHAR(128)
+)
+RETURNS 
+@Users_Recieved TABLE 
+(
+	User_Recieved NVARCHAR(128)
+)
+AS
+BEGIN
+	INSERT @Users_Recieved
+	SELECT UER.User_Recieved FROM UserEmployeeRequests AS UER
+	WHERE UER.User_Sent = @Username
+	ORDER BY UER.User_Sent
+	RETURN 
+END
+GO
