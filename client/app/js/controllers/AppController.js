877f082020c64e46bdec67f7fab8910031a66c9f
Viktor Terziev <Viktor Terziev>
2017-01-30 12:54:22 +0200

    Search content added under search input field.

--- client/app/js/controllers/AppController.js
+++ client/app/js/controllers/AppController.js
@@ -3,6 +3,12 @@
 // The AppController holds the presentation logic for the entire app (common all screens)
 app.controller('AppController', ['$scope', '$location', 'notification',
     function ($scope, $location, notification) {
+        $('#search-found').hide();
+
+        $scope.hide_search = () => {
+            $('#search-found').hide(500);
+        }
+
         $scope.searchCollegues = () => {
             let search = $('#inp-search-colleagues').val();
             console.log('search');
@@ -13,13 +19,24 @@ app.controller('AppController', ['$scope', '$location', 'notification',
                     url: '/search?text=' + search,
                     statusCode: {
                         500: (xhr) => {
-                            console.log(xhr);
                             $location.path('/err').replace();
                             $scope.$apply();
                         }
                     },
                     success: (data) => {
                         console.log(data);
+                        let inner_html = '<ul>';
+                        if (data.length > 0) {
+                            data.forEach((element) => {
+                                inner_html += '<li><a href="#/user/community/' + '">' + element.Username + '</a></li>';
+                            });
+                        } else {
+                            inner_html += '<span>No search results for "' + search + '"</span>';
+                        }
+
+                        inner_html += '</ul>';
+                        $('#search-found-content').html(inner_html);
+                        $('#search-found').show(500);
                     }
                 });
             }
