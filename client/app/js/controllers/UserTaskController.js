9cdca576eee37a52a4c2e33bdd3ce820de0f8eec
petkofff <gpetkofff@abv.bg>
2017-02-22 13:59:24 +0200

    assign user to task search

--- client/app/js/controllers/UserTaskController.js
+++ client/app/js/controllers/UserTaskController.js
@@ -97,6 +97,43 @@ app.controller('UserTaskController', function ($scope, $routeParams, $location, 
             });
     }
 
+    $scope.assignSearch = () => {
+        let search = $('#inp-assign-search').val();
+        $('#inp-assign-search').val('');
+        if (search.length > 0) {
+            ajax.get('/search?text=' + search, statusHandler)
+                .then((data) => {
+                    console.log(data);
+                    let ul = jQuery('<ul/>', {
+                        class: 'list-group'
+                    });
+                    if (data && data.length > 0) {
+                        data.forEach((element) => {
+                            let li = jQuery('<li/>', {
+                                class: 'list-group-item text-center'
+                            }).append(jQuery('<a/>', {
+                                href: '#/user/' + element.Username,
+                                text: element.Username
+                            })).append(jQuery('<button/>', {
+                                class: 'btn btn-default btn-xs',
+                                text: 'Assign'
+                            }));
+                            ul.append(li);
+                        });
+                    } else {
+                        let li = jQuery('<li/>', {
+                            class: 'list-group-item',
+                            text: 'No search results for "' + search + '"'
+                        });
+                        ul.append(li);
+                    }
+
+                    $('#modal-title').text("Found Users");
+                    $('#modal-content').empty().append(ul);
+                });
+        }
+    }
+
     ajax.get('/user/employees', statusHandler)
         .then((data) => {
             data.forEach((element) => {
