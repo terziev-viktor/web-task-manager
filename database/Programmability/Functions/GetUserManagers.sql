d7862862b9a0ed9c8800ea3d7e1d7696f95497af
Viktor Terziev <Viktor Terziev>
2017-02-26 01:46:58 +0200

    Return username and fullname of manager

--- database/Programmability/Functions/GetUserManagers.sql
+++ database/Programmability/Functions/GetUserManagers.sql
@@ -8,10 +8,20 @@ CREATE FUNCTION GetUserManagers
 (	
 	@Username NVARCHAR(128)
 )
-RETURNS TABLE 
-AS
-RETURN 
+RETURNS @Managers TABLE 
 (
-	SELECT um.Manager FROM ManagersEmployees AS um WHERE um.Employee = @Username
+	Username NVARCHAR(128),
+	FullName NVARCHAR(MAX)
 )
+AS
+BEGIN 
+	DECLARE @ManagersTmp AS TABLE(Manager NVARCHAR(128))
+	INSERT @ManagersTmp
+	SELECT um.Manager FROM ManagersEmployees AS um WHERE um.Employee = @Username
+
+	INSERT @Managers(Username, FullName)
+	SELECT U.Username, U.FullName FROM @ManagersTmp AS MT INNER JOIN Users AS U ON U.Username = MT.Manager
+	RETURN 
+END
 GO
+
