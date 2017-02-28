51bb9dbcaee1c0b5741cf3bfe4fd82934fc82022
Viktor Terziev <Viktor Terziev>
2017-02-28 14:25:03 +0200

    Pagination

--- client/app/js/controllers/UserCurrent/UserCurrentTasksController.js
+++ client/app/js/controllers/UserCurrent/UserCurrentTasksController.js
@@ -2,8 +2,10 @@ app.controller('UserCurrentTasksController',
     function ($scope, statusCodeHandler, MaxDescLength, MaxTitleLength, TaskPrioritiesStr, ajax) {
         let statusHandler = statusCodeHandler($scope),
             createdTasksPage = 0,
+            tasksToDoPage = 0,
             createdTasksPageSize = 4,
-            tasksCreatedCount;
+            tasksToDoPageSize = 1,
+            tasksCreatedCount, tasksToDoCount;
         $('.to-show').slideDown("slow");
         $scope.tasksTodo = {
             display: 'loading',
@@ -29,41 +31,48 @@ app.controller('UserCurrentTasksController',
 
         ajax.get('/tasks/todo?getCount=true', statusHandler)
             .then((data) => {
-                $scope.taskTodoCount = data.count.Count;
+                console.log('////////////////////');
+                console.log(data);
+                $scope.taskTodoCount = data.Count;
+                tasksToDoCount = data.Count;
             });
 
         ajax.get('/tasks/created?getCount=true', statusHandler)
             .then((data) => {
-                $scope.taskCreatedCount = data.count.Count;
-                tasksCreatedCount = data.count.Count;
+                console.log('////////////////////');
+                console.log(data);
+                $scope.taskCreatedCount = data.Count;
+                tasksCreatedCount = data.Count;
             });
 
-        ajax.get('/tasks/todo', statusHandler)
-            .then((data) => {
-                data.tasks.forEach(function (element) {
-                    element.priorityLow = element.Priority == 0;
-                    element.priorityMed = element.Priority == 1;
-                    element.priorityHigh = element.Priority == 2;
-                    element.DeadlineFormatted = new Date(element.Deadline).toLocaleString();
-                    if (element.Description.length >= 35) {
-                        element.Description = element.Description.substring(0, 35) + '...';
-                    }
-                    if (element.Title.length > MaxTitleLength) {
-                        element.Title = element.Title.substring(0, MaxTitleLength) + '...';
-                    }
-                }, this);
-                let d = data.tasks.length > 0 ? 'all' : 'none';
-                $scope.tasksTodo = {
-                    display: d,
-                    data: data.tasks
-                };
-            }, (err) => {
-                console.log('error');
-                console.log(err);
-            });
+        function getTasksToDo(from, size) {
+            ajax.get('/tasks/todo?from=' + from + "&size=" + size, statusHandler)
+                .then((data) => {
+                    data.tasks.forEach(function (element) {
+                        element.priorityLow = element.Priority == 0;
+                        element.priorityMed = element.Priority == 1;
+                        element.priorityHigh = element.Priority == 2;
+                        element.DeadlineFormatted = new Date(element.Deadline).toLocaleString();
+                        if (element.Description.length >= 35) {
+                            element.Description = element.Description.substring(0, 35) + '...';
+                        }
+                        if (element.Title.length > MaxTitleLength) {
+                            element.Title = element.Title.substring(0, MaxTitleLength) + '...';
+                        }
+                    }, this);
+                    let d = data.tasks.length > 0 ? 'all' : 'none';
+                    $scope.tasksTodo = {
+                        display: d,
+                        data: data.tasks
+                    };
+                }, (err) => {
+                    console.log('error');
+                    console.log(err);
+                });
+        }
 
-        function getTasksCreated(from, to) {
-            ajax.get('/tasks/created?from=' + from + '&to=' + to, statusHandler)
+        function getTasksCreated(from, size) {
+            ajax.get('/tasks/created?from=' + from + '&size=' + size, statusHandler)
                 .then((data) => {
                     console.log('tasks/created?from&to')
                     console.log(data);
@@ -86,24 +95,59 @@ app.controller('UserCurrentTasksController',
         }
 
         getTasksCreated(createdTasksPage + 1, createdTasksPageSize);
+        getTasksToDo(tasksToDoPage + 1, tasksToDoPageSize);
+
         $scope.nextTasksCreated = () => {
             createdTasksPage += 1;
-            if(createdTasksPage * createdTasksPageSize + 1 > tasksCreatedCount) {
+            let from = createdTasksPage * createdTasksPageSize + 1;
+            if (from > tasksCreatedCount) {
                 createdTasksPage -= 1;
                 return;
             }
-            getTasksCreated(createdTasksPage * createdTasksPageSize + 1, createdTasksPageSize);
-            $('#current-page-tasks-created').val(createdTasksPage + 1);
+            $('#panel-created-tasks').slideUp("slow", () => {
+                getTasksCreated(from, createdTasksPageSize);
+                $('#current-page-tasks-created').val(createdTasksPage + 1);
+                $('#panel-created-tasks').slideDown("slow");
+            });
         }
 
         $scope.previousTasksCreated = () => {
             createdTasksPage -= 1;
-            if(createdTasksPage < 0) {
+            if (createdTasksPage < 0) {
                 createdTasksPage = 0;
                 return;
             }
-            getTasksCreated(createdTasksPage * createdTasksPageSize + 1, createdTasksPageSize);
-            $('#current-page-tasks-created').val(createdTasksPage + 1);
-            
+            $('#panel-created-tasks').slideUp("slow", () => {
+                getTasksCreated(createdTasksPage * createdTasksPageSize + 1, createdTasksPageSize);
+                $('#current-page-tasks-created').val(createdTasksPage + 1);
+                $('#panel-created-tasks').slideDown("slow");
+            });
+        }
+
+        $scope.previousTasksToDo = () => {
+            tasksToDoPage -= 1;
+            if (tasksToDoPage < 0) {
+                tasksToDoPage = 0;
+                return;
+            }
+            $('#panel-todo-tasks').slideUp("slow", () => {
+                getTasksToDo(tasksToDoPage * tasksToDoPageSize, tasksToDoPageSize);
+                $('#current-page-tasks-todo').val(tasksToDoPage + 1);
+                $('#panel-todo-tasks').slideDown("slow");
+            });
+        }
+
+        $scope.nextTasksToDo = () => {
+            tasksToDoPage += 1;
+            let from = tasksToDoPage * tasksToDoPageSize + 1;
+            if (from > tasksToDoCount) {
+                tasksToDoPage -= 1;
+                return;
+            }
+            $('#panel-todo-tasks').slideUp("slow", () => {
+                getTasksToDo(from, tasksToDoPageSize);
+                $('#current-page-tasks-todo').val(tasksToDoPage + 1);
+                $('#panel-todo-tasks').slideDown("slow");
+            });
         }
     });
