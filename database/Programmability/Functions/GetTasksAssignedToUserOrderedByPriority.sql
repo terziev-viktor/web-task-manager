0d4ec453bd2df25eb9e7abbda25a959551bfcea0
Viktor Terziev <Viktor Terziev>
2017-03-01 16:25:37 +0200

    Queries implementation bug fixed

--- database/Programmability/Functions/GetTasksAssignedToUserOrderedByPriority.sql
+++ database/Programmability/Functions/GetTasksAssignedToUserOrderedByPriority.sql
@@ -52,19 +52,36 @@ BEGIN
 	where u.Username = @Username
 	ORDER BY T.[Priority]
 
+	IF (@To <> -1)
+	BEGIN
 	INSERT @AssignedTasks
-	SELECT TOP (@To) T.TaskId, 
-	T.Title, 
-	T.[Description], 
-	T.Deadline, 
-	T.IsDone, 
-	T.[Priority], 
-	T.Progress, 
-	T.Repeatability, 
-	T.Creator_Username FROM
-	(SELECT ROW_NUMBER() OVER (ORDER BY PRIORITY) AS rownumber, * FROM @Tmp AS t) AS T
+		SELECT TOP (@To) T.TaskId, 
+		T.Title, 
+		T.[Description], 
+		T.Deadline, 
+		T.IsDone, 
+		T.[Priority], 
+		T.Progress, 
+		T.Repeatability, 
+		T.Creator_Username FROM
+		(SELECT ROW_NUMBER() OVER (ORDER BY PRIORITY) AS rownumber, * FROM @Tmp AS t) AS T
+		WHERE rownumber >= @From
+	END
+	ELSE
+	BEGIN
+		INSERT @AssignedTasks
+		SELECT T.TaskId, 
+		T.Title, 
+		T.[Description], 
+		T.Deadline, 
+		T.IsDone, 
+		T.[Priority], 
+		T.Progress, 
+		T.Repeatability, 
+		T.Creator_Username FROM
+		(SELECT ROW_NUMBER() OVER (ORDER BY PRIORITY) AS rownumber, * FROM @Tmp AS t) AS T
 	WHERE rownumber >= @From
-
+	END
 	RETURN
 END
 GO
