2cf9a7d8b30f60e8d60c1443a6d6dfd69ee8bafc
Viktor Terziev <Viktor Terziev>
2017-03-16 20:59:53 +0200

    Handling 409. Sorted by status code

--- client/app/js/services/statusCodeHandler.js
+++ client/app/js/services/statusCodeHandler.js
@@ -2,24 +2,30 @@
 app.service('statusCodeHandler', function (notification, $location) {
     return (scope) => {
         return {
-            500: (xhr) => {
-                notification.error(xhr.responseJSON.msg);
+            200: (xhr) => {
+                if (xhr.responseJSON) {
+                    notification.success(xhr.responseJSON.msg);
+                }
+                if (xhr.msg) {
+                    notification.success(xhr.msg);
+                }
             },
-            403: (xhr) => {
-                notification.warning('Forbiddent symbols (\' -- ) used.');
+            400: (xhr) => {
+                notification.error(xhr.responseJSON.msg);
             },
             401: (xhr) => {
                 notification.info('Signup or login in to your profile first :-)');
                 $location.path('/');
                 scope.$apply();
             },
-            200: (xhr) => {
-                if (xhr.responseJSON) {
-                    notification.success(xhr.responseJSON.msg);
-                }
-                if(xhr.msg) {
-                    notification.success(xhr.msg);
-                }
+            403: (xhr) => {
+                notification.warning('Forbiddent symbols (\' -- ) used.');
+            },
+            409: (xhr) => {
+                notification.error('Conflict: ' + xhr.responseJSON.msg);
+            },
+            500: (xhr) => {
+                notification.error(xhr.responseJSON.msg);
             }
         }
     }
