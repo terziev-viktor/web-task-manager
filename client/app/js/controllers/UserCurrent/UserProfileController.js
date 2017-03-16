bde164861d2a0777fa8a77307fa26b7ccce650d5
Viktor Terziev <Viktor Terziev>
2017-03-16 20:59:20 +0200

    Enable/disable button

--- client/app/js/controllers/UserCurrent/UserProfileController.js
+++ client/app/js/controllers/UserCurrent/UserProfileController.js
@@ -7,16 +7,24 @@ app.controller('UserProfileController',
         navbarHandler.handle();
 
         $scope.updatePassword = () => {
+            $('btnsubmit').prop('disabled', true);
             let oldpass = $('#oldpass').val(),
                 newpass = $('#newpass').val(),
                 confirmpass = $('#confirmpass').val();
+            $('#oldpass').val('');
+            $('#newpass').val('');
+            $('#confirmpass').val('');
             let reqData = {
                 oldpass: oldpass,
                 newpass: newpass,
                 confirm: confirmpass
             }
             console.log(reqData);
-            ajax.post('/user/newpass', reqData, statusHandler);
+            ajax.post('/user/newpass', reqData, statusHandler).then(() => {
+                $('btnsubmit').prop('disabled', false);
+            }, () => {
+                $('btnsubmit').prop('disabled', false);
+            });
         }
 
     });
