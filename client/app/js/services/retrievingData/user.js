2a32a64f84630c4ae2dca566463c5f16365c1436
Viktor Terziev <Viktor Terziev>
2017-03-21 16:37:52 +0200

    Added getColleague method

--- client/app/js/services/retrievingData/user.js
+++ client/app/js/services/retrievingData/user.js
@@ -1,7 +1,7 @@
-app.service('userData', function (ajax, $q) {
+app.service('userData', function (ajax, $q, authorization) {
     this.getColleagues = (from, size, statusHandler) => {
         let deferred = $q.defer();
-        ajax.get('/user/colleagues?from=' + from + "&size=" + size, statusHandler)
+        ajax.get('/user/colleagues?relational=1&from=' + from + "&size=" + size, statusHandler)
             .then((data) => {
                 deferred.resolve(data);
             }, (err) => {
@@ -24,6 +24,18 @@ app.service('userData', function (ajax, $q) {
     this.getManagers = (from, size, statusHandler) => {
         let deferred = $q.defer();
         ajax.get('/user/managers?from=' + from + '&size=' + size, statusHandler)
+            .then((data) => {
+                deferred.resolve(data);
+            }, (err) => {
+                deferred.reject(err);
+            });
+        return deferred.promise;
+    }
+
+    this.getColleague = (username, statusHandler) => {
+        let deferred = $q.defer();
+        let currentUser = authorization.getUser();
+        ajax.get('/user/colleague?username=' + username, statusHandler)
             .then((data) => {
                 deferred.resolve(data);
             }, (err) => {
