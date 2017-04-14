f30972723439c2b032959139b5da0b797c70a760
Georgi Petkoff <gppetkoff@abv.bg>
2017-04-14 15:45:36 +0300

    dynamic progressbar, database.ps1 updated

--- database/database.ps1
+++ database/database.ps1
@@ -3,14 +3,17 @@
 #
 # Usage:
 # cd \path\to\database.sh
-# powershell -File database.ps1 [server] [user] [password]
-# example: powershell -File database.ps1 localhost SA pass123
+# powershell -File database.ps1 [server] [user] [password] [delete database parameter]
+# example: powershell -File database.ps1 localhost SA pass123   #  will NOT delete the database prior to running the init scripts
+# example: powershell -File database.ps1 localhost SA pass123 deldb   # will delete the database prior to running the init scripts
+# 
+
 
 $server=$args[0]
 $user=$args[1]
 $pass=$args[2]
 
-if($args[3] -eq "delldb"){
+if($args[3] -eq "deldb"){
     echo "Deleting Database WebTaskManagerDb if it exists..."
     sqlcmd -S $server -U $user -P $pass -Q "DROP DATABASE WebTaskManagerDb" 
 }
